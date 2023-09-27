4ca14e22efc09c5b193f1284c36938ca416eb342
Autumn <batorkittiescoding@gmail.com>
2023-09-27 18:29:14 -0400

    learning DOM

---
+++ main.js
@@ -0,0 +1,13 @@
+const colorsList = document.getElementById('colors'); //this will get the colors id from the html
+
+console.log(colorsList); //this will print the colors in the colorsList
+
+//this is how you select using the getElementById body method 
+
+//you can also use the querySelector method to select the colors id
+
+const colorsList1 = document.querySelector('#colors'); //this will get the colors id from the html
+
+// console.log(colorsList1); //this will print the colors in the colorsList
+
+//the # stands for id and the document stands for the html document
